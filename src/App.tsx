f1879a9b26dbd56c9af73839de6b116da4a97b1c
Kamalesh-Dev-Web <kamalsurea619@gmail.com>
2022-07-28 13:11:52 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+
+//TYpes
+
+export interface JokeType {
+  categories: string[];
+  created_at: string;
+  icon_url: string;
+  id: string;
+  updated_at: string;
+  url: string;
+  value: string;
+}
+
+// const empty = {
+//   categories: [""],
+//   created_at: "",
+//   icon_url: "",
+//   id: "",
+//   updated_at: "",
+//   url: "",
+//   value: "",
+// };
 
 function App() {
+  const [joke, setJoke] = useState<JokeType[]>([]);
+
+  useEffect(() => {
+    fetch("https://api.chucknorris.io/jokes/search?query=sea")
+      .then((response) => response.json())
+      .then((res) => setJoke(res.result.slice(10)))
+      .catch((err) => console.log(err));
+  }, []);
+  console.log(joke);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{ fontFamily: "Bungee Shade", color: "orange" }}>
+        Chuck Norris Jokes
+      </h1>
+      {joke.length <= 0 ? (
+        <h1>Loading....</h1>
+      ) : (
+        joke.map((jo: JokeType, index) => {
+          return (
+            <div key={jo.id}>
+              <h4>
+                {index + 1}.{jo.value}
+              </h4>
+            </div>
+          );
+        })
+      )}
     </div>
   );
 }
